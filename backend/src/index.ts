c787fdb75ace790a1717f8ab3601d9bd7f8c6510
kikiola <oluwatoby007@gmail.com>
2026-08-16 09:32:43 +0100

    chore(backend): resolve merge conflicts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,15 @@
 import express from 'express';
 import commitmentsRouter from './routes/commitments';
 import reputationRouter from './routes/reputation';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 // Mount the routers
